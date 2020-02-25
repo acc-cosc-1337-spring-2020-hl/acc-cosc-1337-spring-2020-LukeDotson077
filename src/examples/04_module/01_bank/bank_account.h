b9d829357fa3327dbfde968e5f21468d368f5274
LukeDotson077 <luke.dotson@g.austincc.edu>
2020-02-24 19:03:08 -0600

    bank_account example and test cases

--- src/examples/04_module/01_bank/bank_account.h
+++ src/examples/04_module/01_bank/bank_account.h
@@ -1,1 +1,12 @@
 //bank_account.h
+class BankAccount
+{
+public:
+	BankAccount(int b) : balance{ b } {};
+	int get_balance()const { return balance; }
+	void deposit(int amount);
+	void withdrawl(int amount);
+private:
+	int balance;
+};
+//The balance variable in the class scope is NOT the same variable as the balance variable in main.
--- test/examples_test/04_module_test/04_module_tests.cpp
+++ test/examples_test/04_module_test/04_module_tests.cpp
@@ -1,7 +1,41 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "bank_account.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Test BankAccount get_balance")
+{
+	BankAccount account(150);
+
+	REQUIRE(account.get_balance() == 150);
+}
+
+TEST_CASE("Test BankAccount deposit")
+{
+	BankAccount account(150);
+	REQUIRE(account.get_balance() == 150);
+
+	account.deposit(-50);
+	REQUIRE(account.get_balance() == 150);
+
+	account.deposit(50);
+	REQUIRE(account.get_balance() == 200);
+}
+
+TEST_CASE("Test BankAccount withdrawl")
+{
+	BankAccount account(150);
+	REQUIRE(account.get_balance() == 150);
+
+	account.withdrawl(-5);
+	REQUIRE(account.get_balance() == 150);
+
+	account.withdrawl(50);
+	REQUIRE(account.get_balance() == 100);
+
+	account.withdrawl(101);
+	REQUIRE(account.get_balance() == 100);
+}
